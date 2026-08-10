e957cc0b25a25c1e75db3d32f09e7659ed4f6fb9
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-08-09 23:59:52 -0300

    Se conecto las rutas de usuarios y tareas

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import taskRoutes from "./src/routes/taskRoutes.js"
+import userRoutes from "./src/routes/userRoutes.js"
 
 dotenv.config()
 
 import { sequelize } from "./src/config/database.js"
 
 const app = express()
+app.use(express.json());
+app.use("user", userRoutes)
+app.use("task", taskRoutes)
 
 console.log(process.env.PORT)
 const port = process.env.PORT
